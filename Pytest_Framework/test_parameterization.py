6049bb419c25df4a63aebbc33bad2d157e4c62b7
Muhindhar <muhindharsv27@gmail.com>
2026-06-08 21:06:02 +0530

    08/06

--- Pytest_Framework/test_parameterization.py
+++ Pytest_Framework/test_parameterization.py
@@ -1,5 +1,5 @@
 import pytest
 @pytest.mark.smoke
-@pytest.mark.parametrize("test_input,exp",[(1,3),(3,6),(5,7)])
+@pytest.mark.parametrize("test_input,exp",[(1,3),(3,5),(5,7)])
 def test_addnum(test_input,exp):
     assert test_input+2 == exp
