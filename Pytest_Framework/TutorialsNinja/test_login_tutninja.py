c0e783d28a0534197347646e0536e65263cbd911
Muhindhar <muhindharsv27@gmail.com>
2026-06-10 00:02:33 +0530

    ninja

--- Pytest_Framework/TutorialsNinja/test_login_tutninja.py
+++ Pytest_Framework/TutorialsNinja/test_login_tutninja.py
@@ -8,8 +8,9 @@ from read_config import get_config
 @pytest.mark.usefixtures("setup_teardown")
 class TestLogin:
     def test_valid_login(self):
-        wait = WebDriverWait(self.driver, 10)
-        self.driver.find_element(By.XPATH, "//span[normalize-space()='My Account']").click()
+        wait = WebDriverWait(self.driver, 20)
+        my_account = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='My Account']")))
+        self.driver.execute_script("arguments[0].click();",my_account)
         self.driver.find_element(By.XPATH,"//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']",).click()
         self.driver.find_element(By.ID, "input-email").send_keys(get_config("valid login cred", "mail"))
         self.driver.find_element(By.ID, "input-password").send_keys(get_config("valid login cred", "pass"))
@@ -19,7 +20,8 @@ class TestLogin:
 
     def test_invalid_login(self):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.find_element(By.XPATH, "//span[normalize-space()='My Account']").click()
+        my_account = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='My Account']")))
+        self.driver.execute_script("arguments[0].click();",my_account)
         self.driver.find_element(By.XPATH,"//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']",).click()
         self.driver.find_element(By.ID, "input-email").send_keys(get_config("invalid login cred", "mail"))
         self.driver.find_element(By.ID, "input-password").send_keys(get_config("invalid login cred", "pass"))
