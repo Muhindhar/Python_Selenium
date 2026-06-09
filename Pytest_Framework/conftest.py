2cc17743cb370663dacc783ca4eba50b80fc5562
Muhindhar <muhindharsv27@gmail.com>
2026-06-09 14:12:49 +0530

    allure

--- Pytest_Framework/conftest.py
+++ Pytest_Framework/conftest.py
@@ -1,31 +1,18 @@
 import pytest
 from selenium import webdriver
 
-
-
-'''
 @pytest.fixture()
 def test_setup_teardown(request):
-    #global driver
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(5)
-    driver.get("https://tutorialsninja.com/demo/")
-    request.cls.driver = driver
-    yield
-    driver.quit()
-    '''
-    
-    
-@pytest.fixture()
-def test_setup_teardown(request):
-    browser = request.param
+
+    browser = getattr(request, "param", "chrome")
+
     if browser == "chrome":
         driver = webdriver.Chrome()
     elif browser == "firefox":
         driver = webdriver.Firefox()
     elif browser == "edge":
         driver = webdriver.Edge()
+
     driver.maximize_window()
     driver.implicitly_wait(5)
     driver.get("https://tutorialsninja.com/demo/")
--- Pytest_Framework/test_SoftAssertions.py
+++ Pytest_Framework/test_SoftAssertions.py
@@ -8,19 +8,31 @@ driver.implicitly_wait(5)
 driver.get("https://tutorialsninja.com/demo/")
 
 def test_invalid():
-    driver.find_element(By.XPATH,"//input[@placeholder='Search']").send_keys("honda")
-    driver.find_element(By.XPATH,"//button[@class='btn btn-default btn-lg']").click()
-    exp = "There is no product" 
-    actual = driver.find_element(By.XPATH,"//p[contains(text(),'There is no product')]").text
-    #check.equal(actual, exp, "Message validation failed")
-    #print("Execution continues after failure")
+
+    driver.find_element(
+        By.XPATH,
+        "//input[@placeholder='Search']"
+    ).send_keys("honda")
+
+    driver.find_element(
+        By.XPATH,
+        "//button[@class='btn btn-default btn-lg']"
+    ).click()
+
+    exp = "There is no product"
+
+    actual = driver.find_element(
+        By.XPATH,
+        "//p[contains(text(),'There is no product')]"
+    ).text
+
+    check.not_equal(actual, exp, "not_equal validation")
+    print("not_equal passed")
+
+    check.is_true(actual != exp, "is_true validation")
+    print("is_true passed")
+
+    check.is_false(actual == exp, "is_false validation")
+    print("is_false passed")
+
     driver.quit()
-    check.not_equal(actual, exp, "Message validation failed")
-    print("Execution continues after failure")
-    
-    check.is_true(actual, exp, "Message validation failed")
-    print("istrue is passed")
-    
-    check.is_false(actual, exp, "Message validation failed")
-    print("IS false is passed")
-    
