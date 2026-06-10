fc20d72fd6bc85827160b516fb93caa4188f30bb
Muhindhar <muhindharsv27@gmail.com>
2026-06-10 07:44:32 +0530

    tutorials ninja

--- Pytest_Framework/TutorialsNinja/read_config.py
+++ Pytest_Framework/TutorialsNinja/read_config.py
@@ -7,16 +7,8 @@ def get_config(category,key):
     
 from configparser import ConfigParser
 import os
-
 def get_config(category, key):
-
     config = ConfigParser()
-
-    config_path = os.path.join(
-        os.path.dirname(__file__),
-        "config.ini"
-    )
-
+    config_path = os.path.join(os.path.dirname(__file__),"config.ini")
     config.read(config_path)
-
     return config.get(category, key)
--- Pytest_Framework/TutorialsNinja/test_orderProd.py
+++ Pytest_Framework/TutorialsNinja/test_orderProd.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Utilities import excelutilites
 from read_config import get_config
+from Selenium_Practice.AutomationExercise import zipcode
 
 
 @pytest.mark.parametrize("order",excelutilites.get_data(r"D:\Python_Selenium\Pytest_Framework\TutorialsNinja\ExcelFiles\order.xlsx","Sheet1"))
@@ -13,9 +14,9 @@ class Test_order:
         wait = WebDriverWait(self.driver, 20)
         my_account = wait.until(EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='My Account']")))
         self.driver.execute_script("arguments[0].click();",my_account)
-        self.driver.find_element(By.LINK_TEXT,"Login").click()
-        self.driver.find_element(By.ID,"input-ex`mail").send_keys(get_config("valid login cred", "mail"))
-        self.driver.find_element(By.ID,"input-password").send_keys(get_config("valid login cred", "pass"))
+        self.driver.find_element(By.XPATH,"//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']").click()
+        self.driver.find_element(By.XPATH,"//input[@id='input-email']").send_keys(get_config("valid login cred", "mail"))
+        self.driver.find_element(By.XPATH,"//input[@id='input-password']").send_keys(get_config("valid login cred", "pass"))
         self.driver.find_element(By.XPATH,"//input[@value='Login']").click()
 
         wait.until(EC.visibility_of_element_located((By.XPATH, "//h2[text()='My Account']")))
@@ -26,15 +27,15 @@ class Test_order:
         search_box.send_keys(order[0])
         self.driver.find_element(By.XPATH,"//button[@class='btn btn-default btn-lg']").click()
         wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[@class='product-thumb']//h4/a)[1]"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "button-cart"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='button-cart']"))).click()
         wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Shopping Cart')]"))).click()
-        wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Checkout"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "button-payment-address"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "button-shipping-address"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@title='Checkout']//i[@class='fa fa-share']"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='button-payment-address']"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='button-shipping-address']"))).click()
         wait.until(EC.element_to_be_clickable((By.ID, "button-shipping-method"))).click()
 
-        wait.until(EC.element_to_be_clickable((By.NAME, "agree"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "button-payment-method"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "button-confirm"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@name='agree']"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='button-payment-method']"))).click()
+        wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='button-confirm']"))).click()
         actual = wait.until(EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Your order has been placed')]"))).text
         assert actual == "Your order has been placed!"
